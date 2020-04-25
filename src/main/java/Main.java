92c1e3b61dea66150e71364386792270c91b6f39
lukaszkomenda <loukas.komenda@gmail.com>
2020-04-25 10:29:30 +0200

    initial menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner S;
+
     public static void main(String[] args) {
-        
+
+        menu();
+
+        S = new Scanner(System.in);
+
+        while (true){
+            int option = S.nextInt();
+            switch (option){
+                case 1:
+                    System.out.println("Nazwa funkcjonalności nie została zaimplementowana");
+                    break;
+                case 2:
+                    System.out.println("Nazwa funkcjonalności nie została zaimplementowana");
+                    break;
+                case 3:
+                    System.out.println("Nazwa funkcjonalności nie została zaimplementowana");
+                    break;
+                default:
+                    System.out.println("Wybierz poprawną opcję.");
+            }
+        }
+    }
+
+    private static void menu() {
+        System.out.println("Witaj w bibliotece! Co mogę dla ciebie zrobić? \n" +
+                "1) Wyświetl wszystkie książki \n" +
+                "2) Dodaj książkę \n" +
+                "3) Usuń książkę");
     }
 }
