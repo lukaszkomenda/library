04a5ed62947abf19643a72adc50ae600c0529cf0
lukaszkomenda <loukas.komenda@gmail.com>
2020-04-27 14:19:22 +0200

    add feature: validate ISBN

--- src/main/java/Model/Book.java
+++ src/main/java/Model/Book.java
@@ -13,7 +13,11 @@ public class Book {
         this.id = idcounter++;
         this.author = author;
         this.title = title;
-        this.isbn = isbn;
+        try {
+            this.isbn = validateISBN(isbn);
+        } catch (BookException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
@@ -40,4 +44,84 @@ public class Book {
                 "\ntitle: " + title +
                 "\nisbn: " + isbn;
     }
+
+    private String validateISBN(String isbn) throws BookException {
+        if (isIsbnCorrectByLength(isbn)){
+            if (isIsbnCorrectByFirstNumbers(isbn)){
+                if (isIsbnCorrectByChecksum(isbn)){
+                    return isbn;
+                } else {
+                    throw new BookException("Błędny ISBN - nieodpowiednia suma kontrolna." +
+                            "\n" + id +
+                            "\n" + author +
+                            "\n" + title +
+                            "\n"+ isbn + "\n");
+                }
+            } else {
+                throw new BookException("Błędny ISBN - nieodpowiednie pierwsze trzy cyfry.\n" +
+                        "\n" + id +
+                        "\n" + author +
+                        "\n" + title +
+                        "\n"+ isbn + "\n");
+            }
+        } else {
+            throw new BookException("Błędny ISBN - nieodpowiednia liczba cyfr.\n" +
+                    "\n" + id +
+                    "\n" + author +
+                    "\n" + title +
+                    "\n"+ isbn + "\n");
+        }
+    }
+
+    private boolean isIsbnCorrectByLength(String isbn) {
+        if (isbn.length() == 13) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isIsbnCorrectByFirstNumbers(String isbn) {
+        String firstNumbers = "978";
+
+        if (firstNumbers.equals(isbn.substring(0,3))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isIsbnCorrectByChecksum(String isbn) {
+
+        char[] array = isbn.toCharArray();
+        int result1 = 0;
+        int checksum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            char sign = array[i];
+            boolean isSign = Character.isDigit(sign);
+
+            if (isSign) {
+                int value = Character.getNumericValue(sign);
+
+                if (i == array.length - 1) {
+                    checksum += value;
+                } else if (i % 2 == 0) {
+                    result1 += value;
+                } else {
+                    result1 = result1 + (value * 3);
+                }
+            }
+        }
+        double result2 = result1 % 10;
+        double result3 = 10 - result2;
+        double result4 = result3 % 10;
+
+        if (result4 == checksum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
---
+++ src/main/java/Model/BookException.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class BookException extends Exception {
+    public BookException(String message) {
+        super(message);
+    }
+}
